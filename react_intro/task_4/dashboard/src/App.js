16275d8a5915c6f61d62fe47f80a94f7eb023d03
Benja1512 <bontiveros15@gmail.com>
2024-03-14 15:25:22 -0500

    fixed: code Deploy your application to GitHub Pages using gh-pages branch and create-react-app

--- react_intro/task_4/dashboard/src/App.js
+++ react_intro/task_4/dashboard/src/App.js
@@ -1,29 +1,41 @@
-import logo from './holberton-logo.jpg';
+// import logo from './logo.svg';
+import holbertonLogo from './holberton-logo.jpg';
 import './App.css';
 import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className="App">
+      <div className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="logo" />
+
         <h1>School dashboard</h1>
-      </header>
-      <body className='App-body'>
-        <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>Email:</label>
-        <input type='email' name='email' id='email' />
+            </div>
+                    
+                    <div className="App-body">
+                      <p>
+                        Login to access the full dashboard
+                      </p>
 
-        <label htmlFor='password'>Password:</label>
-        <input type='password' name='password' id='password' />
+                      <label for="email">email</label><br />
+                      <input type="email" id="email" name="email"></input><br />
+                      <br />
 
-        <button>OK</button>
-      </body>
-      <footer className='App-footer'>
-      <p>{`Copyright ${getFullYear()} - ${getFooterCopy(true)}`}</p>
-      </footer>
+                      <label for="password">password</label><br />
+                      <input type="password" id="password" name="password"></input><br />
+                      <br />
+
+                      <button>OK</button>
+                    </div>
+
+            <div className="App-footer">
+
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
