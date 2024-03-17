31903eb5d4f7091178306d88b4369357e25a9624
Benja1512 <bontiveros15@gmail.com>
2024-03-17 15:17:15 -0500

    feat: this is task_5

--- react_intro/task_5/dashboard/src/App/App.js
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from '../assets/logo.jpg'
+import { getFullYear, getFooterCopy } from '../utils/utils'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	render() {
+		return (
+			<div className="App">
+	  			<header className="App-header">
+		  			<img src={logo} className="App-logo" alt="logo" />
+		  			<h1>School dashboard</h1>
+	  			</header>
+	 			<body className="App-body">
+		 		 	<p>Login to access the full dashboard</p>
+					<label htmlFor="email" onClick={() => {
+						// select corresponding input
+						document.getElementById('password').focus();
+					}}>Email</label>
+					<input type="email" id="email" />
+					<label htmlFor="password" onClick={() => {
+						// select corresponding input
+						document.getElementById('password').focus();
+					}}>Password</label>
+					<input type="password" id="password" />
+					<button>OK</button>
+	  			</body>
+				<footer className="App-footer">
+					<p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+				</footer>
+			</div>
+		)
+	}
 }
-
-export default App;
