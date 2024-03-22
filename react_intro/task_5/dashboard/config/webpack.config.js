af62136b252e1dbab96ab2011a7d690493bbc618
Benja1512 <bontiveros15@gmail.com>
2024-03-22 12:27:42 -0500

    fixed: task_5 generating a bundle.js

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,48 +1,42 @@
 const path = require('path');
 
-
 module.exports = {
-	mode: 'development',
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve('./dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true,
-							disable: true
-						},
-					},
-				],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react']
-					}
-				}
-			}
-		]
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: path.resolve('./dist'),
-		compress: true,
-		port: 8564,
-	},
+    // APP ENTRY POINT
+    entry: './src/index.js',
+    mode: 'development', // Set mode to 'development'
+
+    // OUTPUT DIRECTORY
+    output: {
+        filename: 'bundle.js', // Nombre del archivo bundle generado
+        path: path.resolve(__dirname, './dist') 
+    },
+
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            }
+        ]
+    },
+
+    // DEV SERVER ENTRY POINT
+    devServer: {
+        hot: true,
+        compress: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        historyApiFallback: true
+    },
 };
