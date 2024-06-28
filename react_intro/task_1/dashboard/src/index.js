511c931d31c28a3009dcdac5ea085c1f34fa9934
Benja1512 <benjamin.ontiveros@upsjb.edu.pe>
2024-06-28 18:07:06 -0500

    feat:Embedding expressions, functions

--- react_intro/task_1/dashboard/src/index.js
+++ react_intro/task_1/dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Notifications } from './Notifications';
+import { Notifications } from './Notifications.js';
 
 ReactDOM.render(
   <React.StrictMode>
