063ff3d97914e9fa9e95a8bf7b0b68e8fbadc64c
Benja1512 <benjamin.ontiveros@upsjb.edu.pe>
2024-06-29 07:17:10 -0500

    feat:Reorganize the files

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,89 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/index.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../dist'),
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, '../dist'),
-        hot: true,
-    },
-    module: { // <-- This is where the typo was previously
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("./dist"),
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve("./dist"),
+    compress: true,
+    port: 8564,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
             },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images/',
-                        },
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                            },
-                            // Optimize PNG
-                            optipng: {
-                                enabled: true,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4,
-                            },
-                            // Optimize GIF
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // Optimize SVG
-                            svgo: {
-                                plugins: [
-                                    {
-                                        removeViewBox: false,
-                                    },
-                                    {
-                                        removeEmptyAttrs: false,
-                                    },
-                                ],
-                            },
-                        },
-                    },
-                ],
-            },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../dist/index.html'),
-            filename: 'index.html',
-        }),
+      },
     ],
-    devtool: 'inline-source-map',
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+  },
 };
--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,8 +1,14 @@
-import './style.css';
-import exampleImage from './image.png';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import Notifications from "./Notifications/Notifications";
 
-console.log('Hello, Webpack!');
-
-const img = document.createElement('img');
-img.src = exampleImage;
-document.body.appendChild(img);
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
