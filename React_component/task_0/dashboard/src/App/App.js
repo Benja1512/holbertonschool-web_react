1e48af708fc7dff8637e0a3643b2ab8aa59c6cef
Benja1512 <benjamin.ontiveros@upsjb.edu.pe>
2024-06-29 09:28:45 -0500

    feat: react component

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,55 +1,51 @@
-import React, { Component } from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import CourseList from "../CourseList/CourseList";
-import Footer from "../Footer/Footer";
-import PropTypes from "prop-types";
-import { getLatestNotification } from "../utils/utils";
-import "./App.css";
+import './App.css';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import PropTypes from 'prop-types';
 
-const listCourses = [
-  { id: 1, name: "ES6", credit: 60 },
-  { id: 2, name: "Webpack", credit: 20 },
-  { id: 3, name: "React", credit: 40 },
-];
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
 
-const listNotifications = [
-  { id: 1, type: "default", value: "New course available" },
-  { id: 2, type: "urgent", value: "New resume available" },
-  { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-];
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
+class App extends React.Component {
   render() {
+    // instantiate vars from props
     const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    const htmlObj = getLatestNotification();
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New course available' },
+      { id: 3, type: 'urgent', html: htmlObj },
+    ]
+
     return (
       <>
-        <Notifications listNotifications={listNotifications} />
+        <Notifications displayDrawer={ false } listNotifications={ listNotifications } />
         <div className="App">
           <Header />
-        </div>
-        <div className="App-body">
-          {!isLoggedIn ? <Login /> : <CourseList listCourses={listCourses} />}
-        </div>
-        <div className="App-footer">
+          { isLoggedIn ? <CourseList listCourses={ listCourses } /> : <Login /> }
           <Footer />
         </div>
       </>
-    );
+    )
   }
 }
-
-App.defaultProps = {
-  isLoggedIn: false,
-};
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
 };
 
-export default App;
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default hot(module)(App);
