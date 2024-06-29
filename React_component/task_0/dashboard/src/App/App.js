265d1944b6c1d5cf49872e2866f49a10af953276
Benja1512 <benjamin.ontiveros@upsjb.edu.pe>
2024-06-29 09:39:29 -0500

    feat: modifed

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,18 +1,55 @@
-// App.js (functional component)
-import React from 'react';
-import './App.css';
+import React, { Fragment, Component } from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+
+class App extends Component {
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      {
+        id: 3,
+        type: 'urgent',
+        html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' }
+      }
+    ];
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
+      <Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          <div className='App-body'>
+            {!isLoggedIn && <Login />}
+            {isLoggedIn && <CourseList listCourses={listCourses} />}
+          </div>
+          <Footer />
         </div>
+      </Fragment>
     );
+  }
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool
+};
+
+App.defaultProps = {
+  isLoggedIn: false
+};
 
 export default App;
